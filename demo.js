dfd2ba811da6eaeb52a69890244a56df1845c5c5
Kirubakaran <kirubakaranpt.ece@gmail.com>
2025-08-13 11:14:13 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,19 @@
+function off() {
+    var on = document.getElementById("fan");
+    on.style.animationName = "none";
+}
+
+function one(){
+    var on = document.getElementById("fan");
+    on.style.animationDuration = "0.5s";
+}
+
+function two(){
+    var on = document.getElementById("fan");
+    on.style.animationDuration = "0.3s";
+}
+
+function three(){
+    var on = document.getElementById("fan");
+    on.style.animationDuration = "0.1s";
+}
